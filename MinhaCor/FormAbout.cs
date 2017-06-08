4e90cc192aaccb33e1a69e1fee5fcb3bb098dbbe
weastridge <weastridge@users.noreply.github.com>
2017-06-07 21:54:58 -0400

    and made custom resource files too

--- MinhaCor/FormAbout.cs
+++ MinhaCor/FormAbout.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -43,11 +44,17 @@ namespace MinhaCor
                 {
                     //intro
                     StringBuilder sb = new StringBuilder();
+                    
                     sb.Append("MinhaCor (MyColor) is a fun way to show that our own " +
                         "skin color is unique, yet part of a wonderful family of colors.");
                     textBox1.Text = sb.ToString();
 
+
+
                     //culture
+
+                    ResourceManager rm = new ResourceManager("MinhaCor.resources", typeof(Program).Assembly);
+                    textBox1.Text += "\r\n" + rm.GetString("StringHello");
                     labelCulture.Text = "Current Culture is " + 
                         System.Globalization.CultureInfo.CurrentCulture.DisplayName;
                     labelUICulture.Text = "Current User Interface Culture is " + 
